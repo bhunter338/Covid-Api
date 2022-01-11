bf65eb91964d06579bf75348e19534aa8df3e2b9
Onur Bilginer <bilgineronur@gmail.com>
2022-01-11 16:55:15 +0300

    swagger ui setup

--- Data/CovidDataRepo.cs
+++ Data/CovidDataRepo.cs
@@ -1,4 +1,6 @@
+using System.Data;
 using Covid_Api.Models;
+using HtmlAgilityPack;
 
 namespace Covid_Api.Data
 {
@@ -6,6 +8,16 @@ namespace Covid_Api.Data
     {
         public TotalData GetDailyTotalData(string country)
         {
+            string url = "https://www.worldometers.info/coronavirus/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url).DocumentNode;
+
+            var headers = doc.SelectNodes("//tr/th");
+
+            DataTable table = new DataTable();
+            foreach (HtmlNode header in headers)
+                table.Columns.Add(header.InnerText);
+
             return new TotalData { Name = country, TotalConfirmed = 10, TotalRecovered = 20, TotalDeaths = 40 };
         }
 
